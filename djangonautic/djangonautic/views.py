896a55566eeddf93ca6501e3c81699ecf91bc466
Vinayak333-tech <mishravinayak839@gmail.com>
2020-09-30 23:22:48 +0530

    edit

---
+++ djangonautic/djangonautic/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request,'about.html')
+    #  return HttpResponse("About")
+
+
+def homepage(request):
+    return render(request,'homepage.html')
+    #  return HttpResponse("Homepage")
